6a29c5ebbaa4f20e7784b5285302cdee73d6944a
dah4k <84741290+dah4k@users.noreply.github.com>
2026-01-11 14:15:45 -0500

    Refactor into module Fsharp.RcHello

--- Program.fs
+++ Program.fs
@@ -1,5 +1,9 @@
 ﻿// Copyright 2025 dah4k
 // SPDX-License-Identifier: EPL-2.0
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+module Fsharp.RcHello
+
+[<EntryPoint>]
+let main args =
+    printfn "Hello from F#"
+    0
